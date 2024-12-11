bc3dee71a269642cc4fe90bb6850d1eb96dc9e24
funniray <funniray10@gmail.com>
2024-12-11 20:35:34 +0000

    Possibly speedup day 11

--- Sources/day11.swift
+++ Sources/day11.swift
@@ -7,6 +7,9 @@ class Day11: Day {
     func run() {
         let data = readInput()
 
+        print(countDigits(2024))
+        print(splitDigits(2024,countDigits(2024)))
+
         var p1 = data
         for _ in 0..<25 {
             p1 = blink(p1)
@@ -22,11 +25,12 @@ class Day11: Day {
     func blink(_ data: [Int: Int]) -> [Int: Int] {
         var res: [Int: Int] = [:]
         for num in data {
-            let digits = countDigits(num.key)
+            let dNum = Double(num.key)
+            let digits = countDigits(dNum)
             if num.key == 0 {
                 appendOrInc(&res, 1, num.value)
-            } else if digits % 2 == 0 {
-                let split = splitDigits(num.key, digits)
+            } else if digits.truncatingRemainder(dividingBy: 2) == 0 {
+                let split = splitDigits(dNum, digits)
                 appendOrInc(&res, split.0, num.value)
                 appendOrInc(&res, split.1, num.value)
             } else {
@@ -44,31 +48,15 @@ class Day11: Day {
         }
     }
 
-    func splitDigits(_ num: Int, _ digits: Int) -> (Int, Int) {
-        if digits % 2 == 1 {print("Got odd number input \(num)")}
-        let power = pow(10.0, Double(digits)/2.0)
-        let left = Int(floor(Double(num)/power))
-        let right = num - Int(Double(left)*power)
+    func splitDigits(_ num: Double, _ digits: Double) -> (Int, Int) {
+        let power = pow(10.0, digits/2.0)
+        let left = Int(floor(num/power))
+        let right = Int(num.truncatingRemainder(dividingBy: power))
         return (left,right)
     }
 
-    func countDigits(_ num: Int) -> Int {
-        if num >= 1_000_000_000_000_000 {print("Number too large \(num)")}
-        if num >= 100_000_000_000_000 {return 15}
-        if num >= 10_000_000_000_000 {return 14}
-        if num >= 1_000_000_000_000 {return 13}
-        if num >= 100_000_000_000 {return 12}
-        if num >= 10_000_000_000 {return 11}
-        if num >= 1_000_000_000 {return 10}
-        if num >= 100_000_000 {return 9}
-        if num >= 10_000_000 {return 8}
-        if num >= 1_000_000 {return 7}
-        if num >= 100_000 {return 6}
-        if num >= 10_000 {return 5}
-        if num >= 1_000 {return 4}
-        if num >= 100 {return 3}
-        if num >= 10 {return 2}
-        return 1
+    func countDigits(_ num: Double) -> Double {
+        return floor(log10(num))+1
     }
 
     func readInput() -> [Int: Int] {
